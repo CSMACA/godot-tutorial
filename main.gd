077b733eb9870a5dca14f14f0c3510c30f29e9f8
Alan Roach <arscience97@gmail.com>
2023-03-09 20:01:56 -0600

    End of Page => https://docs.godotengine.org/en/stable/getting_started/first_2d_game/06.heads_up_display.html

--- main.gd
+++ main.gd
@@ -6,14 +6,19 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
+	get_tree().call_group("mobs", "queue_free")
 	$StartTimer.start()
 	$Player.start($StartPosition.position)
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -45,4 +50,4 @@ func _on_MobTimer_timeout():
 	add_child(mob)
 
 func _ready():
-	new_game()
+	pass
